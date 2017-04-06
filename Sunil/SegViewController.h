565713aa3a7e81695492f58b9588a1fa87942d68
Sunil <sunilkumaryamsani@Sunils-MacBook-Pro.local>
2017-04-06 09:58:47 -0700

    new files added by sunil

---
+++ Sunil/SegViewController.h
@@ -0,0 +1,13 @@
+//
+//  SegViewController.h
+//  Sunil
+//
+//  Created by Sunil  on 4/6/17.
+//  Copyright © 2017 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SegViewController : UIViewController
+
+@end
